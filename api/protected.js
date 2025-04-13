eb552fb4cf79f58fed8182b9a2bbc07967ced614
Ravishka Madhushan <madushanravishka71@gmail.com>
2025-04-13 20:24:53 +0530

    Update protected.js

--- api/protected.js
+++ api/protected.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const referer = req.headers.referer;
 
-  if (!referer || !referer.includes('customized-player.vercel.app')) {
+  if (!referer || !referer.includes('custom-player.vercel.app')) {
     // Redirect to custom 403 page
     return res.redirect(302, '/404.html');
   }
